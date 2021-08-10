6a55354d676dcff590af0fb062d50d56af63be49
Michał Micherda <mmicherdakr@gmail.com>
2021-08-10 18:56:09 +0200

    add change

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
+Link to working project: https://festival-app-api.herokuapp.com/
 ## Available Scripts
 
 In the project directory, you can run:
--- src/components/features/SeatChooser/SeatChooser.js
+++ src/components/features/SeatChooser/SeatChooser.js
@@ -7,7 +7,7 @@ class SeatChooser extends React.Component {
 
   componentDidMount() {
     const { loadSeats, loadSeatsData } = this.props;
-    this.socket = io(process.env.NODE_ENV === 'production' ? process.env.PUBLIC_URL : 'localhost:8000', { transports: ["websocket"] });
+    this.socket = io.connect((process.env.NODE_ENV === 'production') ? '/' : 'http://localhost:8000');
     this.socket.on('seatsUpdated', seats => loadSeatsData(seats));
     loadSeats();
   }
